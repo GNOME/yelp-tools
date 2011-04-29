3dfe77422dd6d83a4cf3b094297ab6560f70ee37
Shaun McCance <shaunm@gnome.org>
2011-04-29 12:15:18 -0400

    Cleaned up old, unused files

--- xml2po/tests/relnotes/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-ALLFILES=`cat XMLFILES`
-XML2PO="../../xml2po/xml2po"
-($XML2PO $ALLFILES | sed 's/"POT-Creation-Date: .*$/"POT-Creation-Date: \\n"/' | diff -u release-notes.pot -) || echo "Problem with POT extraction"
-for i in $ALLFILES; do
-    ($XML2PO -p el.po $i | diff -u el/$i -) || echo "Problem with merging $i"
-done
